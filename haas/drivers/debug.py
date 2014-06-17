514271fde2a2d30b84111bce5a74da0f20a1b76e
jlbernard <bernard.logan4@gmail.com>
2014-06-17 13:32:14 -0400

    driver with basic functionality

--- haas/drivers/debug.py
+++ haas/drivers/debug.py
@@ -1,22 +1,14 @@
-"""A switch driver for testing
+"""
+A switch driver for testing
 
-	 set_access_vlan info will be logged to the console
+set_access_vlan debug level info will be logged to console
 """
 
 import logging, sys
 
 
 def set_access_vlan(port, vlan_id):
-	
-	# route log handler 
-	console = logging.StreamHandler(sys.stdout)
-	console.setLevel(logging.DEBUG)
 
-	# init logger and vlan info message
-	switchLogger = logging.getLogger('test_switch')
-	switchLogger.addHandler(console)
-	switchLogger.setLevel(logging.DEBUG)
-	vlan_info = 'VLAN_port = '+ str(port) +', VLAN_id = '+ str(vlan_id)
-
-	# log info and debug level information
-	switchLogger.debug(vlan_info)
+	logging.basicConfig(level=logging.DEBUG)
+	logger = logging.getLogger(__name__)
+	logger.debug('A hopefully helpful message')
